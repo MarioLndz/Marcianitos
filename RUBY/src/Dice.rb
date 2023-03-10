aa123f4b5e0dd462fdda7da226a4ddb1d9510423
MarioLndz <mariolindez@gmail.com>
2023-03-10 11:19:44 +0100

    Clase Dice empezada

--- RUBY/src/Dice.rb
+++ RUBY/src/Dice.rb
@@ -3,21 +3,30 @@
 module DeepSpace
 	class Dice
 		def initialize
+			# Atributos de instancia
 			@NHANGARSPROB = 0.25
 			@NSHIELDSPROB = 0.25
 			@NWEAPONSPROB = 0.33
 			@FIRSTSHOTPROB = 0.5
+			@generator = Random.new
 			
-			@generator = rand(0)
 		end
 		
 		attr_reader :generator
+		
+		def initWithNHangars
+			retorno = 1
+			
+			if (@generator.rand() < @NHANGARSPROB)
+				retorno = 0
+			end
+			
+			return (retorno)
+			
+		end
 	end
 	
-	print "Prueba\n"
-	obj = Dice.new
-	num = obj.generator
+	dice=Dice.new
 	
-	print generator
-
+	print dice.initWithNHangars
 end
