51d87dcff93abfebc0fedb7aa44bdf7452397c59
mario10ugr <100713048+mario10ugr@users.noreply.github.com>
2023-03-29 14:01:45 +0200

    Update GameUniverse.rb

--- RUBY/src/GameUniverse.rb
+++ RUBY/src/GameUniverse.rb
@@ -19,8 +19,95 @@ module DeepSpace
 			@turns = 0
 			
 			@dice = Dice.new
+			
+			@spaceStations = []
+			
+			@currentStationIndex = -1
+			
+			@currentEnemy = EnemyStarShip.new
 		end
 		
+		def haveAWinner()
+			
+			return @currentStation.getNMedals >= @@WIN
+		end
+		
+		def discardHangar()
+			
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  ##no estoy muy seguro de que esa sea la sintaxis  
+																					  ##en todos los discard
+				@currentStation.discardHangar()
+			end
+		end
+		
+		def discardShieldBooster(i)
+		
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.discardShieldBooster(i)
+			end
+		end
+		
+		def discardShieldBoosterInHangar(i)
+		
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.discardShieldBoosterInHangar(i)
+			end
+		end
+		
+		def discardShieldBoosterInHangar(i)
+		
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.discardShieldBoosterInHangar(i)
+			end
+		end
+		
+		def discardWeapon(i)
+		
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.discardWeapon(i)
+			end
+		end
+		
+		def discardWeaponInHangar(i)
+		
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.discardWeaponInHangar(i)
+			end
+		end
+		
+		def getState()
+		
+			return @gameState.state       ## se hace así cuando está el attreader en lugar del get() ??
+		end
+		
+		def getUIversion()
+		
+			return GameUniverseToUI.new(self)
+			
+		end
+		
+		def mountShieldBooster(i)
+			
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.mountShieldBooster(i)
+			end
+			
+		end
+		
+		def mountWeapon(i)
+			
+			if @gameState == GameState::Init || @gameState == GameState::AFTERCOMBAT  
+			
+				@currentStation.mountWeapon(i)
+			end
+			
+		end 	
 		
 		# Para la proxima practica
 		def init (names)
